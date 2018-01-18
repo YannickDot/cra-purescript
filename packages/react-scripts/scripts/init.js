8d38979536044dbb23ee4553f3f51626e5cd9f7c
YannickDot <yannick.levif@gmail.com>
2018-01-18 22:55:02 +0100

    added create-react-class package

--- packages/react-scripts/scripts/init.js
+++ packages/react-scripts/scripts/init.js
@@ -120,7 +120,7 @@ module.exports = function(
     args = ['install', '--save', verbose && '--verbose'].filter(e => e)
     argsDev = ['install', '--save-dev', verbose && '--verbose'].filter(e => e)
   }
-  args.push('react', 'react-dom')
+  args.push('react', 'react-dom', 'create-react-class')
   argsDev.push('purs-loader')
 
   // Install additional template dependencies, if present
